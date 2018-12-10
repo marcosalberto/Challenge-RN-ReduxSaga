9abf59ab3935d07c0690a1a1216c745290cb4531
Marcos Alberto <marcos@marcosalberto.com.br>
2018-12-10 15:06:39 -0300

    Add redux, react-redux and redux-saga package

--- android/app/src/main/java/com/fotonreduxchallenge/MainApplication.java
+++ android/app/src/main/java/com/fotonreduxchallenge/MainApplication.java
@@ -5,6 +5,8 @@ import android.app.Application;
 import com.facebook.react.ReactApplication;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.horcrux.svg.SvgPackage;
+import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
+import com.horcrux.svg.SvgPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -25,6 +27,8 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNGestureHandlerPackage(),
+            new SvgPackage(),
             new RNGestureHandlerPackage(),
             new SvgPackage()
       );
